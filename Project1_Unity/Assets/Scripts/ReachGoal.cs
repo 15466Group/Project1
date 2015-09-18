08ddd01da4cb6f15c20342f35a141b872d359c56
dominicxcliu <dominicxcliu@gmail.com>
2015-09-17 21:10:13 -0400

    added reachgoal, wander tweaks

--- Project1_Unity/Assets/Scripts/ReachGoal.cs
+++ Project1_Unity/Assets/Scripts/ReachGoal.cs
@@ -4,22 +4,76 @@ using System.Collections;
 public class ReachGoal : MonoBehaviour {
 
 	public GameObject target;
+	public Animation anim;
 
 	private Vector3 acceleration;
 	private Vector3 velocity;
 	private float accMag;
-	private float speedMax;
+	public float maxSpeed;
 
-	private float lambda; //angle from target to character
-	private float psi; //direction character is currently facing
+	private Vector3 targetAccel;
+	private Vector3 targetPosition;
+
+	private float idleSpeed;
+	public float walkingSpeed;
+
+	private float maxRadsDelta;
+	private float maxMagDelta;
+
+	private float smooth;
 
 	// Use this for initialization
 	void Start () {
-	
+
+		//as accMag increases, accelerates faster
+		accMag = 500.0f;
+		velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+		acceleration = new Vector3 (0.0f, 0.0f, 0.0f);
+		targetAccel = new Vector3 (0.0f, 0.0f, 0.0f);
+		targetPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+		maxMagDelta = 100.0f;
+		//as maxRadsDelta increases, guy turns towards goal faster
+		maxRadsDelta = Mathf.Deg2Rad * 10.0f;
+		idleSpeed = 0.0f;
+		anim.CrossFade ("idle");
+		smooth = 5.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//want him to accelerate towards the target.position
+		transform.position += velocity * Time.deltaTime;
+		velocity = velocity + acceleration * Time.deltaTime;
+		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
+		
+		acceleration = Vector3.RotateTowards (acceleration, targetAccel, maxRadsDelta, maxMagDelta);
+		targetPosition = transform.position + velocity * Time.deltaTime;
+		RotateTo (targetPosition);
+
+
+		targetAccel = new Vector3 (target.transform.position.x - transform.position.x, 0.0f,target.transform.position.z - transform.position.z);
+		targetAccel = targetAccel.normalized;
+		targetAccel = targetAccel * accMag;
+
+
+		float mag = velocity.magnitude;
+		if (mag <= walkingSpeed && mag > idleSpeed) {
+			anim.CrossFade ("Walk");
+		} else if (mag > walkingSpeed) {
+			anim.CrossFade ("Run");
+		} else 
+			anim.CrossFade ("idle");
+	}
+
+	void RotateTo(Vector3 targetPosition){
+		Quaternion destinationRotation;
+		Vector3 relativePosition;
+		relativePosition = targetPosition - transform.position;
+		Debug.DrawRay(transform.position,relativePosition*10,Color.red);
+		Debug.DrawRay(transform.position,targetAccel*10,Color.red);
+		Debug.DrawRay(transform.position,velocity*10,Color.green);
+		Debug.DrawRay(transform.position,acceleration*10,Color.blue);
+		destinationRotation = Quaternion.LookRotation (relativePosition);
+		transform.rotation = Quaternion.Slerp (transform.rotation, destinationRotation, Time.deltaTime * smooth);
 	}
 }
--- Project1_Unity/Assets/Scripts/Wander.cs
+++ Project1_Unity/Assets/Scripts/Wander.cs
@@ -12,57 +12,48 @@ public class Wander : MonoBehaviour {
 	private Vector3 targetAccel;
 	private float maxRadsDelta;
 	private float maxMagDelta;
-	private float maxSpeed;
+	public float maxSpeed;
 
 	private float smooth;
 	private float randomRad;
 	private float idleSpeed;
-	private float walkingSpeed;
+	public float walkingSpeed;
 	private Vector3 targetPosition;
-
-	private float timer;
-
 
 	// Use this for initialization
 	void Start () {
 		accMag = 50.0f;
-		maxSpeed = 20.0f;
 		velocity = new Vector3 (0.0f, 0.0f, 0.0f);
 		acceleration = new Vector3 (0.0f, 0.0f, 0.0f);
 		targetAccel = new Vector3 (0.0f, 0.0f, 0.0f);
 		targetPosition = new Vector3 (0.0f, 0.0f, 0.0f);
 		maxMagDelta = 100.0f;
 		maxRadsDelta = Mathf.Deg2Rad * 1.0f;
-
 		idleSpeed = 0.0f;
-		walkingSpeed = 8.0f;
 		anim.CrossFade ("idle");
 		smooth = 5.0f;
-		timer = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime;
 		//transform.Translate (velocity * Time.deltaTime, Space.World);
 		transform.position += velocity * Time.deltaTime;
 		velocity = velocity + acceleration * Time.deltaTime;
 		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
 
+		acceleration = Vector3.RotateTowards (acceleration, targetAccel, maxRadsDelta, maxMagDelta);
+		targetPosition = transform.position + velocity * Time.deltaTime;
+		RotateTo (targetPosition);
+
 		randomRad = 0.0f;
 		if (targetAccel == acceleration) {
+			//maxRadsDelta = Mathf.Deg2Rad * 1.0f;
+			//accMag = 50.0f;
 			randomRad = Random.Range (0.0f, Mathf.PI * 2.0f);
 			float newx = accMag * Mathf.Cos (randomRad);
 			float newz = accMag * Mathf.Sin (randomRad);
 			targetAccel = new Vector3 (newx, 0.0f, newz);
 		}
-
-		Debug.Log (maxRadsDelta);
-		acceleration = Vector3.RotateTowards (acceleration, targetAccel, maxRadsDelta, maxMagDelta);
-
-		targetPosition = transform.position + velocity * Time.deltaTime;
-
-		RotateTo (targetPosition);
 
 		float mag = velocity.magnitude;
 		if (mag <= walkingSpeed && mag > idleSpeed) {
@@ -71,7 +62,26 @@ public class Wander : MonoBehaviour {
 			anim.CrossFade ("Run");
 		} else 
 			anim.CrossFade ("idle");
+
+		//checkCollide ();
+
+
 	}
+
+//	void checkCollide () {
+//		RaycastHit hit;
+//		Ray collideRay = new Ray(transform.position, transform.forward);
+//
+//		if (Physics.Raycast (collideRay, out hit, viewDistance)) {
+//			accMag = 50000.0f;
+//			maxRadsDelta = Mathf.Deg2Rad * 360.0f;
+//			targetAccel = targetAccel * -1.0f;
+//			velocity = new Vector3();
+//
+//			float dist = hit.distance;
+//		}
+//	
+//	}
 
 	void RotateTo(Vector3 targetPosition){
 		Quaternion destinationRotation;
