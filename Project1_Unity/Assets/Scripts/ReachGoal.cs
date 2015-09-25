30981c2982c13351a60531f6fc17ee8f857ce19c
dominicxcliu <dominicxcliu@gmail.com>
2015-09-25 14:30:51 -0400

    better obstacle avoidance

--- Project1_Unity/Assets/Scripts/ReachGoal.cs
+++ Project1_Unity/Assets/Scripts/ReachGoal.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ReachGoal : MonoBehaviour {
 
-	public GameObject target;
+	public GameObject goal;
 	public Animation anim;
 
 	private Vector3 acceleration;
@@ -26,6 +26,9 @@ public class ReachGoal : MonoBehaviour {
 	private float charWidth;
 	private float rayDist;
 
+
+	private Vector3 tempGoal;
+
 	// Use this for initialization
 	void Start () {
 
@@ -37,7 +40,7 @@ public class ReachGoal : MonoBehaviour {
 		targetPosition = new Vector3 (0.0f, 0.0f, 0.0f);
 		maxMagDelta = 100.0f;
 		//as maxRadsDelta increases, guy turns towards goal faster
-		maxRadsDelta = Mathf.Deg2Rad * 10.0f;
+		maxRadsDelta = Mathf.Deg2Rad * 20.0f;
 		idleSpeed = 0.0f;
 		anim.CrossFade ("idle");
 		smooth = 5.0f;
@@ -64,10 +67,12 @@ public class ReachGoal : MonoBehaviour {
 			RotateTo (targetPosition);
 
 
-		targetAccel = new Vector3 (target.transform.position.x - transform.position.x, 0.0f,target.transform.position.z - transform.position.z);
-		//targetAccel = new Vector3 (transform.position.x - target.transform.position.x, 0.0f, transform.position.z - target.transform.position.z);
-		targetAccel = collisions (targetAccel);
+//		targetAccel = new Vector3 (target.transform.position.x - transform.position.x, 0.0f,target.transform.position.z - transform.position.z);
+//		targetAccel = new Vector3 (transform.position.x - target.transform.position.x, 0.0f, transform.position.z - target.transform.position.z);
+//		targetAccel = collisions (targetAccel);
 //		targetAccel = avoidObjects (targetAccel);
+
+		targetAccel = calculateAcceleration ();
 		targetAccel = targetAccel.normalized;
 		targetAccel = targetAccel * accMag;
 
@@ -90,10 +95,10 @@ public class ReachGoal : MonoBehaviour {
 		Debug.DrawRay(transform.position,targetAccel.normalized*30,Color.red);
 		Debug.DrawRay(transform.position,velocity.normalized*20,Color.green);
 		Debug.DrawRay(transform.position,acceleration.normalized*10,Color.blue);
-		Debug.DrawRay(transform.position - transform.right*charWidth, transform.forward * rayDist, Color.yellow);
-		Debug.DrawRay(transform.position + transform.right*charWidth, transform.forward * rayDist, Color.yellow);
-		Debug.DrawRay(transform.position - transform.right*charWidth, (transform.forward - transform.right) * 1.5f * rayDist, Color.yellow);
-		Debug.DrawRay(transform.position + transform.right*charWidth, (transform.forward + transform.right) * 1.5f * rayDist, Color.yellow);
+		Debug.DrawRay(transform.position - transform.right*charWidth, transform.forward.normalized * rayDist, Color.yellow);
+		Debug.DrawRay(transform.position + transform.right*charWidth, transform.forward.normalized * rayDist, Color.yellow);
+		Debug.DrawRay(transform.position - transform.right*charWidth, (transform.forward - transform.right).normalized * 1.5f * rayDist, Color.yellow);
+		Debug.DrawRay(transform.position + transform.right*charWidth, (transform.forward + transform.right).normalized * 1.5f * rayDist, Color.yellow);
 		//Debug.DrawRay(transform.position, (target.transform.position - transform.position) * 50.0f, Color.yellow);
 		destinationRotation = Quaternion.LookRotation (relativePosition);
 		transform.rotation = Quaternion.Slerp (transform.rotation, destinationRotation, Time.deltaTime * smooth);
@@ -101,8 +106,8 @@ public class ReachGoal : MonoBehaviour {
 
 	void veloCloseToTarget () {
 		float epsilon = 0.7f;
-		float xDistance = Mathf.Abs (transform.position.x - target.transform.position.x);
-		float zDistance = Mathf.Abs (transform.position.z - target.transform.position.z);
+		float xDistance = Mathf.Abs (transform.position.x - goal.transform.position.x);
+		float zDistance = Mathf.Abs (transform.position.z - goal.transform.position.z);
 		float distance = Mathf.Sqrt (xDistance * xDistance + zDistance * zDistance);
 
 		if (distance <= epsilon) {
@@ -113,7 +118,7 @@ public class ReachGoal : MonoBehaviour {
 			maxSpeed = Mathf.Min(Mathf.Pow(1.1f,distance) + 10.0f, originalMaxSpeed);
 		}
 	}
-
+	/*
 	Vector3 avoidObjects(Vector3 acceleration){
 		RaycastHit hitL;
 		RaycastHit hitR;
@@ -133,8 +138,122 @@ public class ReachGoal : MonoBehaviour {
 
 		return acceleration;
 	}
+	*/
 
-	Vector3 collisions(Vector3 acceleration) {
+	/*Vector3 calculateAcceleration() {
+
+		RaycastHit hitL;
+		RaycastHit hitR;
+
+		bool hitLeft = Physics.Raycast (transform.position - transform.right*charWidth, transform.forward, out hitL, rayDist);
+		bool hitRight = Physics.Raycast (transform.position + transform.right*charWidth, transform.forward, out hitR, rayDist);
+		if (Vector3.Distance (transform.position, tempGoal) < 1.0f) {
+			
+		
+
+			if (hitLeft && hitRight) {
+				if (hitL.distance < hitR.distance) {
+					Vector3 somewheretotheright = hitR.point + hitR.normal.normalized * 15.0f + ;
+					tempGoal = somewheretotheright;
+				} else {
+					Vector3 somewheretotheleft = hitL.point + hitL.normal.normalized * 50.0f;
+					tempGoal = somewheretotheleft;
+				}
+			} else if (!hitLeft && hitRight) {
+				Vector3 somewheretotheleft = hitR.point + hitR.normal.normalized * 50.0f;
+				tempGoal = somewheretotheleft;
+			} else if (hitLeft && !hitRight) {
+				Vector3 somewheretotheright = hitL.point + hitL.normal.normalized * 50.0f;
+				tempGoal = somewheretotheright;
+			} else {
+				tempGoal = goal.transform.position;
+			}
+
+		}
+		targetAccel = new Vector3 (tempGoal.x - transform.position.x, 0.0f,tempGoal.z - transform.position.z);
+		return targetAccel;
+	}*/
+
+
+	Vector3 calculateAcceleration () {
+		RaycastHit hitL;
+		RaycastHit hitR;
+		RaycastHit hitLS;
+		RaycastHit hitRS;
+		RaycastHit hitT;
+
+		targetAccel = new Vector3 (goal.transform.position.x - transform.position.x, 0.0f,goal.transform.position.z - transform.position.z);
+
+		bool hitLeft = Physics.Raycast (transform.position - transform.right*charWidth, transform.forward.normalized, out hitL, rayDist);
+		bool hitRight = Physics.Raycast (transform.position + transform.right*charWidth, transform.forward.normalized, out hitR, rayDist);
+		bool hitLeftS = Physics.Raycast (transform.position - transform.right*charWidth, (transform.forward - transform.right).normalized, out hitLS, 1.5f * rayDist);
+		bool hitRightS = Physics.Raycast (transform.position + transform.right*charWidth, (transform.forward + transform.right).normalized, out hitRS, 1.5f * rayDist);
+
+		Debug.DrawRay(transform.position - transform.right*charWidth, transform.forward.normalized * hitL.distance, Color.magenta);
+		Debug.DrawRay(transform.position + transform.right*charWidth, transform.forward.normalized * hitR.distance, Color.magenta);
+		Debug.DrawRay(transform.position - transform.right*charWidth, (transform.forward - transform.right).normalized * hitLS.distance, Color.magenta);
+		Debug.DrawRay(transform.position + transform.right*charWidth, (transform.forward + transform.right).normalized * hitRS.distance, Color.magenta);
+
+		float lDist = (transform.position - transform.right * charWidth - goal.transform.position).magnitude;
+		float rDist = (transform.position + transform.right * charWidth - goal.transform.position).magnitude;
+		bool rightClose = rDist < lDist;
+		float targetDist = Vector3.Distance (transform.position, goal.transform.position);
+		
+		if (!hitLeft && !hitRight && !hitLeftS && !hitRightS) {
+			Debug.Log ("nothing is hitting");
+			return targetAccel;
+		} else if (!hitLeft && !hitRight) {
+			Debug.Log ("gostraight" + hitRight + hitLeft);
+			if (rightClose) {
+				if (!hitRightS || (targetDist <= hitRS.distance)) {
+					//acceleration already set towards target
+					return targetAccel;
+				} else {
+					return transform.forward * (accMag);
+				}
+			} else {
+				if (!hitLeftS || (targetDist <= hitLS.distance)) {
+					return targetAccel;
+				} else {
+					return transform.forward * (accMag);
+				}
+			}
+		} else if (hitLeft || hitRight) {
+			Debug.Log ("asdf-1");
+			if (rightClose) {
+				if (!hitRightS || (targetDist <= hitRS.distance)) {
+					//return Vector3.RotateTowards (acceleration, transform.forward + transform.right, 1.0f, 0.0f);
+					//return targetAccel;
+					return (transform.forward + transform.right).normalized * accMag;
+				} else {
+					if (!hitLeftS || (targetDist <= hitLS.distance)) {
+						return (transform.forward - transform.right).normalized * accMag;
+						//return Vector3.RotateTowards (acceleration, transform.forward - transform.right, 1.0f, 0.0f);
+					} else {
+						return (targetAccel * (-1.0f));
+					}
+				}
+			} else {
+				if (!hitLeftS || (targetDist <= hitLS.distance)) {
+					//return Vector3.RotateTowards (acceleration, transform.forward - transform.right, 1.0f, 0.0f);
+					Debug.Log ("asdf0");
+					return (transform.forward - transform.right).normalized * accMag;
+				} else {
+					if (!hitRightS || (targetDist <= hitRS.distance)) {
+						Debug.Log ("asdf1");
+						return (transform.forward + transform.right).normalized * accMag;
+						//return Vector3.RotateTowards (acceleration, transform.forward + transform.right, 1.0f, 0.0f);
+					} else {
+						return (targetAccel * (-1.0f));
+					}
+				}
+			}
+		} else {
+			return targetAccel;
+		}
+	}
+
+	/*Vector3 collisions(Vector3 acceleration) {
 		RaycastHit hitL;
 		RaycastHit hitR;
 		RaycastHit hitLS;
@@ -207,6 +326,6 @@ public class ReachGoal : MonoBehaviour {
 			return acceleration;
 		}
 
-	}
+	}*/
 
 }
